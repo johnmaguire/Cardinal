94c6fe02a3cdbb34cecab5fe2735870167b79b15
John Maguire <contact@johnmaguire.me>
2014-08-16 22:48:53 -0400

    finishes conversion of core config to new config classes

--- cardinal.py
+++ cardinal.py
@@ -7,7 +7,7 @@ import argparse
 from getpass import getpass
 
 from twisted.internet import reactor
-from cardinal.config import ConfigParser
+from cardinal.config import ConfigParser, ConfigSpec
 from cardinal.bot import CardinalBotFactory
 
 # If this file is being run directly, set the logging level, load config file,
@@ -16,31 +16,88 @@ if __name__ == "__main__":
     # Set logging to info by default
     logging.basicConfig(level=logging.INFO)
 
-    parser = ConfigParser()
+    # Define the ArgumentParser for CLI options
+    arg_parser = argparse.ArgumentParser(description="""
+Cardinal IRC bot
+
+A Python/Twisted-powered modular IRC bot. Aimed to be simple to use, simple
+to develop. For information on developing plugins, visit the project page
+below.
+
+https://github.com/JohnMaguire/Cardinal
+""", formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    arg_parser.add_argument('-n', '--nickname', metavar='nickname', help='nickname to connect as')
+    arg_parser.add_argument('--password', action='store_true', help='set this flag to get a password prompt for identifying')
+    arg_parser.add_argument('-i', '--network', metavar='network', help='network to connect to')
+    arg_parser.add_argument('-o', '--port', type=int, metavar='port', help='network port to connect to')
+    arg_parser.add_argument('-s', '--ssl', action='store_true', help='you must set this flag for SSL connections')
+    arg_parser.add_argument('-c', '--channels', nargs='*', metavar='channel', help='list of channels to connect to on startup')
+    arg_parser.add_argument('-p', '--plugins', nargs='*', metavar='plugin', help='list of plugins to load on startup')
+
+    # Define the config spec and create a parser for our internal config
+    spec = ConfigSpec()
+    spec.add_option('nickname', basestring, 'Cardinal')
+    spec.add_option('password', basestring, None)
+    spec.add_option('network', basestring, 'irc.freenode.net')
+    spec.add_option('port', int, 6667)
+    spec.add_option('ssl', bool, False)
+    spec.add_option('channels', list, ['#bots'])
+    spec.add_option('plugins', list, [
+        'ping',
+        'help',
+        'admin',
+        'join_on_invite',
+        'urls',
+        'calculator',
+        'lastfm',
+        'remind',
+        'weather',
+        'youtube'
+    ])
+    parser = ConfigParser(spec)
 
     # First attempt to load config.json for config options
     logging.info("Looking for and attempting to load config.json")
-    config = parser.load_config('config.json')
+    parser.load_config('config.json')
 
     # Parse command-line arguments last, as they should override both project
     # defaults and the user config (if available)
     logging.info("Parsing command-line arguments")
-    argparser = parser.generate_argparser()
-    args   = argparser.parse_args()
+    args = arg_parser.parse_args()
+
+    # If SSL is set to false, set it to None (small hack - action 'store_true'
+    # in arg_parse defaults to False. False instead of None will overwrite our
+    # config settings.)
+    if not args.ssl:
+        args.ssl = True
 
     # If the password flag was set, let the user safely type in their password
     if args.password:
-        password = getpass('NickServ password: ')
+        args.password = getpass('NickServ password: ')
     else:
-        password = config['password']
+        args.password = None
+
+    # Merge the args into the config object
+    logging.info("Merging command-line arguments into config")
+    config = parser.merge_argparse_args_into_config(args)
 
     # Instance a new factory, and connect with/without SSL
-    factory = CardinalBotFactory(args.network, args.channels, args.nickname, password, args.plugins)
-    if not args.ssl:
-        reactor.connectTCP(args.network, args.port, factory)
+    logging.info("Initializing Cardinal factory")
+    factory = CardinalBotFactory(config['network'], config['channels'],
+        config['nickname'], config['password'], config['plugins'])
+
+    if not config['ssl']:
+        logging.info(
+            "Connecting over plaintext to %s:%d" % (config['network'], config['port'])
+        )
+        reactor.connectTCP(config['network'], config['port'], factory)
     else:
+        logging.info(
+            "Connecting over SSL to %s:%d" % (config['network'], config['port'])
+        )
         from twisted.internet import ssl
-        reactor.connectSSL(args.network, args.port, factory, ssl.ClientContextFactory())
+        reactor.connectSSL(config['network'], config['port'], factory, ssl.ClientContextFactory())
 
     # Run the Twisted reactor
     reactor.run()
--- cardinal/config.py
+++ cardinal/config.py
@@ -1,6 +1,7 @@
 import logging
 import json
 import argparse
+import inspect
 
 # Hardcoded defaults in case the config file is missing and no command-line
 # options are specified
@@ -25,6 +26,62 @@ DEFAULT_PLUGINS = (
 #    'event_examples',
 )
 
+class ConfigSpec(object):
+    """A class used to create a config spec for ConfigParser"""
+
+    options = {}
+    """A dictionary holding tuples of the options"""
+
+    def add_option(self, name, type, default = None):
+        # Name must be a string
+        if not isinstance(name, basestring):
+            raise ValueError("Name must be a string")
+
+        if not inspect.isclass(type):
+            raise ValueError("Type must be a class")
+
+        # Ensure that the name is in UTF-8 encoding
+        name = name.encode('utf-8')
+
+        self.options[name] = (type, default)
+
+    def return_value_or_default(self, name, value):
+        """Validates an option and returns it or the default
+
+        If the value passed in passes validation for its option's type, then it
+        will be returned. Otherwise, the default will. This is used for
+        validation.
+
+        Keyword arguments:
+          name  -- The name of the option to validate for.
+          value -- The value to validate.
+
+        Return:
+          string -- The value passed in or the option's default value.
+
+        """
+        if name not in self.options:
+            raise KeyError("%s is not a valid option" % name)
+
+        # Separate the type and default from the tuple
+        type, default = self.options[name]
+
+        # Return the default if the value passed in was wrong, otherwise return
+        # the value passed in
+        if not isinstance(value, type):
+            if value is not None:
+                logging.warning(
+                    "Value passed in for option %s was invalid -- ignoring" % name
+                )
+            else:
+                logging.debug(
+                    "No value set for option %s -- using default" % name
+                )
+
+            return default
+        else:
+            return value
+
 class ConfigParser(object):
     """A class to make parsing of JSON configs easier.
 
@@ -34,24 +91,24 @@ class ConfigParser(object):
     command-line input.)
 
     """
-    config = {}
 
-    def _option_is_valid(self, config, option, type):
-        """Returns whether an option exists in the config and the correct type
+    config = {}
+    """A dictionary containing config values as we learn them"""
+
+    spec = None
+    """A ConfigSpec object passed into the constructor"""
+
+    def __init__(self, spec):
+        """Initializes ConfigParser with a ConfigSpec
 
         Keyword arguments:
-          config -- A JSON-decoded object containing a config
-          option -- The name of an option to validate
-          type   -- A Python object to check if option matches the type of
-
-        Returns:
-          bool -- True if validation succeeds, false if not
+          spec -- Should be a built ConfigSpec
 
         """
-        if option in config and isinstance(config[option], type):
-            return True
+        if not isinstance(spec, ConfigSpec):
+            raise ValueError("Spec must be a config spec")
 
-        return False
+        self.spec = spec
 
     def _convert_json(self, json_object, called_by_self=False):
         """Converts json.load() or json.loads() return to UTF-8.
@@ -89,8 +146,8 @@ class ConfigParser(object):
 
         Takes a file path, attempts to decode its contents from JSON, then
         validate known config options to see if they can safely be loaded in.
-        If they can't, the default option (DEFAULT_* constants) are used in
         their place. The final merged dictionary object is saved to the
+        If they can't, the default value from the config spec is used in the
         instance and returned.
 
         Keyword arguments:
@@ -103,7 +160,7 @@ class ConfigParser(object):
         # Attempt to load and parse the config file
         try:
             f = open(file, 'r')
-            config = self._convert_json(json.load(f))
+            json_config = self._convert_json(json.load(f))
             f.close()
         # File did not exist or we can't open it for another reason
         except IOError:
@@ -116,60 +173,45 @@ class ConfigParser(object):
                 "Invalid JSON in %s, (using defaults / command-line values" % file
             )
         else:
-            # File was loaded successfully, now we validate all the options and set
-            # the corresponding constant if they appear to be valid
-            if not self._option_is_valid(config, 'nickname', basestring):
-                config['nickname'] = DEFAULT_NICKNAME
-            if not self._option_is_valid(config, 'password', basestring):
-                config['password'] = DEFAULT_PASSWORD
-            if not self._option_is_valid(config, 'network', basestring):
-                config['network']  = DEFAULT_NETWORK
-            if not self._option_is_valid(config, 'port', int):
-                config['port']     = DEFAULT_PORT
-            if not self._option_is_valid(config, 'ssl', bool):
-                config['ssl']      = DEFAULT_SSL
-            if not self._option_is_valid(config, 'channels', list):
-                config['channels'] = DEFAULT_CHANNELS
-            if not self._option_is_valid(config, 'plugins', list):
-                config['plugins']  = DEFAULT_PLUGINS
+            # For every option, 
+            for option in self.spec.options:
+                try:
+                    # If the option wasn't defined in the config, ensure default
+                    if option not in json_config:
+                        json_config[option] = None
 
-        self.config = config
-        return config
+                    self.config[option] = self.spec.return_value_or_default(option, json_config[option])
+                except KeyError:
+                    logging.warning("Option %s not in spec -- ignored" % option)
 
-    def generate_argparser(self):
-        """Generates the help file and command-line option parser.
+        # If we didn't load the config earlier, or there was nothing in it...
+        if self.config == {} and self.spec.options != {}:
+            for option in self.spec.options:
+                # Grab the default
+                self.config[option] = self.spec.options[option][1]
+
+        return self.config
+
+    def merge_argparse_args_into_config(self, args):
+        """Merges the args returned by argparse.ArgumentParser into the config.
 
         Keyword arguments:
-          config -- A config dictionary from load_config() for default values
+          args -- The args object returned by argsparse.parse_args()
 
         Return:
-          object -- A built ArgumentParser object
+          dict -- Dictionary object of the entire config
 
         """
-        parser = argparse.ArgumentParser(description="""
-    Cardinal IRC bot
+        for option in self.spec.options:
+            try:
+                # If the value exists in args and is set, then update the
+                # config's value
+                value = getattr(args, option)
+                if value is not None:
+                    self.config[option] = value
+            except AttributeError:
+                logging.debug(
+                    "Option %s not in CLI arguments -- not updated" % name
+                )
 
-    A Python/Twisted-powered modular IRC bot. Aimed to be simple to use, simple
-    to develop. For information on developing plugins, visit the project page
-    below.
-
-    https://github.com/JohnMaguire/Cardinal
-        """, formatter_class=argparse.RawDescriptionHelpFormatter)
-
-        parser.add_argument('-n', '--nickname', default=self.config['nickname'],
-                            metavar='nickname', help='nickname to connect as')
-        parser.add_argument('--password', default=None, action='store_true',
-                            help='set this flag to get a password prompt for identifying')
-        parser.add_argument('-i', '--network', default=self.config['network'],
-                            metavar='network', help='network to connect to')
-        parser.add_argument('-o', '--port', default=self.config['port'], type=int,
-                            metavar='port', help='network port to connect to')
-        parser.add_argument('-s', '--ssl', default=self.config['ssl'], action='store_true',
-                            help='you must set this flag for SSL connections')
-        parser.add_argument('-c', '--channels', default=self.config['channels'], nargs='*',
-                            metavar='channel', help='list of channels to connect to on startup')
-        parser.add_argument('-p', '--plugins', default=self.config['plugins'], nargs='*',
-                            metavar='plugin', help='list of plugins to load on startup')
-
-        return parser
-
+        return self.config
