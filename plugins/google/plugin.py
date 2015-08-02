fcacedf09dfe0452c801f89df53152f812b0f7ff
John Maguire <jmaguire@barracuda.com>
2015-08-02 12:07:38 -0400

    Update Google plugin for coding style
     * Made some PEP-8 adjustments
     * Added whitespace
     * Brought comments to separate lines
     * Switched to % string formatting

--- plugins/google/plugin.py
+++ plugins/google/plugin.py
@@ -1,56 +1,58 @@
-from bs4 import BeautifulSoup
 import requests
 import re
-#python3
-#from urllib.parse import quote_plus
-#python2
 from urllib import quote_plus
 
+from bs4 import BeautifulSoup
+
+
 class GoogleSearch(object):
-    def get_search_result(self, cardinal, user, channel, msg):
-        #gets search string from message, and makes it url safe
+    """Original author: Adam 'Flarf' Straub"""
+
+    def search(self, cardinal, user, channel, msg):
+        # gets search string from message, and makes it url safe
         try:
             search_string = msg.split(' ', 1)[1:]
-            if len(search_string) < 1: #makes sure there is a value for <search string>
-                cardinal.sendMsg(channel, 'Syntax: {0} <search string>'.format(msg.split(' ')[0]))
-                return
+            if len(search_string) < 1:
+                return cardinal.sendMsg(channel, 'Syntax: .google <query>')
+
             search_string = " ".join(search_string)
             search_string = quote_plus(search_string)
         except IndexError:
-            cardinal.sendMsg(channel, 'Syntax: google <search string>')
-            return
-        
+            return cardinal.sendMsg(channel, 'Syntax: .google <query>')
+
         try:
-            #gets the html of the goole search page, and passes it in to beautiful soup
-            google_search = requests.get('https://www.google.com/search?q={0}'.format(search_string))
-            soup = BeautifulSoup(google_search.content,'html.parser')
-            #google search result urls are really weird, this pulls the proper url out of google's <a> tags
-            url= re.compile(r'.*(https?:\/\/.+?)\&')
+            # gets the html of the goole search page, and passes it in to beautiful soup
+            google_search = requests.get('https://www.google.com/search?q=%s' % search_string)
+            soup = BeautifulSoup(google_search.content, 'html.parser')
+
+            # google search result urls are really weird, this pulls the proper url out of google's <a> tags
+            url = re.compile(r'.*(https?:\/\/.+?)\&')
             ols = soup.findAll("ol")
-            #iterates through <ol> elements on the page looking for one that looks like
-            #<ol>
-            #  <li class="g">
-            #      <div class="r">
-            #           <a> <----- this is the link you're looking for
-            #      <div class="s">
-            #           #other stuff we don't care about, but this div doesn't exist for add or news elements,
-            #               but The div.r exists for those elements.
+
+            # iterates through <ol> elements on the page looking for one that looks like
+            # <ol>
+            #   <li class="g">
+            #       <div class="r">
+            #            <a> <----- this is the link you're looking for
+            #       <div class="s">
+            #            # other stuff we don't care about, but this div doesn't exist for add or news elements,
+            #              but The div.r exists for those elements.
             for olist in ols:
-            	for result in olist.findAll("li",{"class":"g"}): #finds all elements the are likely search results
-            		if result.find("div",{"class":"s"}): #finds all elements that contain <div class="s"> and are therfore a search result
-            			href = result.find("a")['href']
-            			result = url.match(href).group(1)
-            			cardinal.sendMsg(channel,"top result is: ({0})".format(result))
-            			return
-            			
-            cardinal.sendMsg(channel, "Could find a search result on requested page")
-            return
-        
+                # finds all elements the are likely search results
+                for result in olist.findAll("li", {"class": "g"}):
+                    # finds all elements that contain <div class="s"> and are therfore a search result
+                    if result.find("div", {"class": "s"}):
+                        href = result.find("a")['href']
+                        result = url.match(href).group(1)
+
+                        return cardinal.sendMsg(channel, "Top result is: %s" % result)
+
+            return cardinal.sendMsg(channel, "Couldn't find any results for your query :(")
         except:
-            cardinal.sendMsg(channel, "Could not sucesfully find a result")
-            
-    get_search_result.commands = ['lmgtfy','google','search-for']
-    get_search_result.help = ['Returns the url of the top result for a given string']
+            return cardinal.sendMsg(channel, "Couldn't find any results for your query :(")
+
+    search.commands = ['lmgtfy', 'google', 'search-for']
+    search.help = ['Returns the URL of the top result for a given search query']
 
 def setup():
     return GoogleSearch()
