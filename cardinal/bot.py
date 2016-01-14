e467479f664208964ba8857b00b6a4b66614b128
Conny Sjöblom <biohzn@kapsi.fi>
2016-01-14 10:20:07 +0200

    Creates storage directory if not found

--- cardinal/bot.py
+++ cardinal/bot.py
@@ -507,6 +507,10 @@ class CardinalBotFactory(protocol.ClientFactory):
                     os.path.dirname(os.path.realpath(sys.argv[0])),
                     storage
                 )
+            if not os.path.exists(self.storage_path):
+                self.logger.debug("Storage path does not exist, creating it..")
+                os.makedirs(self.storage_path)
+
             self.logger.debug("Storage path set to %s" % self.storage_path)
 
         # Register SIGINT handler, so we can close the connection cleanly
