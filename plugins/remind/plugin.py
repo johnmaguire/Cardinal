f1a712d88bf5f4c1eedf3300470d99de92ab11d4
John Maguire <contact@johnmaguire.me>
2016-01-19 20:37:08 -0500

    Remove Twisted dependency in remind plugin

--- plugins/remind/plugin.py
+++ plugins/remind/plugin.py
@@ -1,23 +1,26 @@
-from twisted.internet import reactor
+import threading
+
+from cardinal.decorators import command, help
+
 
 class RemindPlugin(object):
+    @command('remind')
+    @help("Sends a reminder after a set time.")
+    @help("Syntax: .remind <minutes> <message>")
     def remind(self, cardinal, user, channel, msg):
         message = msg.split(None, 2)
         if len(message) < 3:
             cardinal.sendMsg(channel, "Syntax: .remind <minutes> <message>")
             return
-        
-        try:
-            reactor.callLater(60 * int(message[1]), cardinal.sendMsg, user.group(1), message[2])
-            cardinal.sendMsg(channel, "%s: You will be reminded in %d minutes." % (user.group(1), int(message[1])))
-        except ValueError:
-            cardinal.sendMsg(channel, "You did not give a valid number of minutes to be reminded in.")
-        except AssertionError:
-            cardinal.sendMsg(channel, "You did not give a valid number of minutes to be reminded in.")
 
-    remind.commands = ['remind']
-    remind.help = ["Sets up a reminder, where the bot will message the user after a predetermined time.",
-                   "Syntax: .remind <minutes> <message>"]
+        timer = threading.Timer(60 * int(message[1]),
+                                cardinal.sendMsg, (user.group(1), message[2]))
+        timer.start()
+
+        cardinal.sendMsg(channel,
+                         "%s: You will be reminded in %d minutes." %
+                         (user.group(1), int(message[1])))
+
 
 def setup():
     return RemindPlugin()
