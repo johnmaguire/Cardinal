889f6a3b05761c705cf0f9db8d6d5623b17620a9
John Maguire <contact@johnmaguire.me>
2019-08-23 15:52:27 -0400

    Modernize URLs plugin
    - Fix class/instance members
    - Handle `fire()` Deferred correctly
    - Use `regex()` decorator

--- plugins/urls/plugin.py
+++ plugins/urls/plugin.py
@@ -1,5 +1,5 @@
 # coding: iso-8859-15
-
+from __future__ import absolute_import, print_function, division
 import re
 import urllib2
 import socket
@@ -7,51 +7,50 @@ import HTMLParser
 import logging
 from datetime import datetime
 
+from twisted.internet import defer
+
 from cardinal.decorators import regex
 
-URL_REGEX = re.compile(r"(?:^|\s)((?:https?://)?(?:[a-z0-9.\-]+[.][a-z]{2,4}/?)(?:[^\s()<>]*|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\))+(?:\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'\".,<>?]))", flags=re.IGNORECASE|re.DOTALL)
-TITLE_REGEX = re.compile(r'<title(\s+.*?)?>(.*?)</title>', flags=re.IGNORECASE|re.DOTALL)
+URL_REGEX = re.compile(r"(?:^|\s)((?:https?://)?(?:[a-z0-9.\-]+[.][a-z]{2,4}/?"
+                       r")(?:[^\s()<>]*|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\)"
+                       r")+(?:\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\)|[^\s`!()"
+                       r"\[\]{};:\'\".,<>?]))",
+                       flags=re.IGNORECASE | re.DOTALL)
+TITLE_REGEX = re.compile(r'<title(\s+.*?)?>(.*?)</title>',
+                         flags=re.IGNORECASE | re.DOTALL)
 
 
 class URLsPlugin(object):
-    logger = None
-    """Logging object for URLsPlugin"""
-
-    timeout = 10
+    TIMEOUT = 10
     """Timeout in seconds before bailing on loading page"""
 
-    read_bytes = 524288
+    READ_BYTES = 524288
     """Bytes to read before bailing on loading page (512KB)"""
 
-    lookup_cooloff = 10
+    LOOKUP_COOLOFF = 10
     """Timeout in seconds before looking up the same URL again"""
-
-    last_url = None
-    """Holds the last URL looked up, for cooloff"""
-
-    last_url_at = None
-    """Holds time last URL was looked up, for cooloff"""
 
     def __init__(self, cardinal, config):
         # Initialize logger
         self.logger = logging.getLogger(__name__)
 
-        # Only check config if it exists
-        if config is None:
-            return
+        # Holds the last URL looked up, for cooloff
+        self.last_url = None
 
-        if 'timeout' in config:
-            self.timeout = config['timeout']
+        # Holds time last URL was looked up, for cooloff
+        self.last_url_at = None
 
-        if 'read_bytes' in config:
-            self.read_bytes = config['read_bytes']
+        # If config doesn't exist, use an empty dict
+        config = config or {}
 
-        if 'lookup_cooloff' in config:
-            self.lookup_cooloff = config['lookup_cooloff']
+        self.timeout = config.get('timeout', self.TIMEOUT)
+        self.read_bytes = config.get('read_bytes', self.READ_BYTES)
+        self.lookup_cooloff = config.get('lookup_cooloff', self.LOOKUP_COOLOFF)
 
         cardinal.event_manager.register('urls.detection', 2)
 
     @regex(URL_REGEX)
+    @defer.inlineCallbacks
     def get_title(self, cardinal, user, channel, msg):
         # Find every URL within the message
         urls = re.findall(URL_REGEX, msg)
@@ -62,33 +61,38 @@ class URLsPlugin(object):
                 url = "http://" + url
 
             if (url == self.last_url and self.last_url_at and
-                (datetime.now() - self.last_url_at).seconds < self.lookup_cooloff):
-                return
+                    (datetime.now() - self.last_url_at).seconds <
+                    self.lookup_cooloff):
+                defer.returnValue(None)
 
             self.last_url = url
             self.last_url_at = datetime.now()
 
             # Check if another plugin has hooked into this URL and wants to
             # provide information itself
-            hooked = cardinal.event_manager.fire('urls.detection', channel, url)
-            if hooked:
-                return
+            hooked = yield cardinal.event_manager.fire(
+                'urls.detection', channel, url)
 
-            # Attempt to load the page, timing out after a default of ten seconds
+            if hooked:
+                defer.returnValue(None)
+
+            # FIXME: Replace with Twisted call
             try:
                 o = urllib2.build_opener()
+                # User agent helps combat some bot checks
                 o.addheaders = [
                     ('User-agent', 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36')
                 ]
                 f = o.open(url, timeout=self.timeout)
             except Exception, e:
                 self.logger.exception("Unable to load URL: %s" % url)
-                return
+                defer.returnValue(None)
 
             # Attempt to find the title
             content_type = f.info()['content-type']
-            if not (('text/html' in content_type) or ('text/xhtml' in content_type)):
-                return
+            if not ('text/html' in content_type or
+                    'text/xhtml' in content_type):
+                defer.returnValue(None)
             content = f.read(self.read_bytes)
             f.close()
 
