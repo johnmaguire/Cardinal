25afc1368e4a71df5e0d5abeb2e09efd314a9654
John Maguire <contact@johnmaguire.me>
2019-09-01 17:11:38 -0400

    Finish unit tests for cardinal.plugins
    More work towards #61

---
+++ cardinal/fixtures/fake_plugins/command_raises_exception/plugin.py
@@ -0,0 +1,15 @@
+from cardinal.decorators import command
+
+
+class TestCommandRaisesExceptionPlugin(object):
+    def __init__(self):
+        self.command_calls = []
+
+    @command('command')
+    def command(self, *args):
+        self.command_calls.append(args)
+        raise Exception()
+
+
+def setup():
+    return TestCommandRaisesExceptionPlugin()
---
+++ cardinal/fixtures/fake_plugins/commands/plugin.py
@@ -0,0 +1,24 @@
+from cardinal.decorators import command, regex
+
+
+class TestCommandsPlugin(object):
+    def __init__(self):
+        self.command1_calls = []
+        self.command2_calls = []
+        self.regex_command_calls = []
+
+    @command(['command1', 'command1_alias'])
+    def command1(self, *args):
+        self.command1_calls.append(args)
+
+    @command('command2')
+    def command2(self, *args):
+        self.command2_calls.append(args)
+
+    @regex('^regex')
+    def regex_command(self, *args):
+        self.regex_command_calls.append(args)
+
+
+def setup():
+    return TestCommandsPlugin()
