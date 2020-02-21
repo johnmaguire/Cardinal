bcec07a816a547b251195de1fae068f7a3b2f445
John Maguire <contact@johnmaguire.me>
2020-02-21 18:24:39 -0500

    Use reactor.callLater() in remind plugin

--- plugins/remind/plugin.py
+++ plugins/remind/plugin.py
@@ -1,9 +1,12 @@
-import threading
+from twisted.internet import error, reactor
 
 from cardinal.decorators import command, help
 
 
 class RemindPlugin(object):
+    def __init__(self):
+        self.call_ids = []
+
     @command('remind')
     @help("Sends a reminder after a set time.")
     @help("Syntax: .remind <minutes> <message>")
@@ -13,13 +16,19 @@ class RemindPlugin(object):
             cardinal.sendMsg(channel, "Syntax: .remind <minutes> <message>")
             return
 
-        timer = threading.Timer(60 * int(message[1]),
-                                cardinal.sendMsg, (user.group(1), message[2]))
-        timer.start()
+        self.call_ids.append(reactor.callLater(60 * int(message[1]),
+                                cardinal.sendMsg, user.group(1), message[2]))
 
         cardinal.sendMsg(channel,
                          "%s: You will be reminded in %d minutes." %
                          (user.group(1), int(message[1])))
+
+    def close(self):
+        for call_id in call_ids:
+            try:
+                call_id.cancel()
+            except error.AlreadyCancelled:
+                pass
 
 
 def setup():
