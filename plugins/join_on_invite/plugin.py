d6ef4aa5dfe9e4317f1752fe6ee771b2d42e39c3
John Maguire <contact@johnmaguire.me>
2020-02-25 01:35:08 -0500

    Add ability to rejoin on kick
    Closes #152

--- plugins/join_on_invite/plugin.py
+++ plugins/join_on_invite/plugin.py
@@ -4,11 +4,24 @@ from cardinal.decorators import event
 class InviteJoinPlugin(object):
     """Simple plugin that joins a channel if an invite is given."""
 
+    def __init__(self, cardinal, config):
+        self.rejoin_on_kick = False
+        if config:
+            self.rejoin_on_kick = config.get('rejoin_on_kick', False)
+
     @event('irc.invite')
     def join_channel(self, cardinal, user, channel):
         """Callback for irc.invite that joins a channel"""
         cardinal.join(channel)
 
+    @event('irc.kick')
+    def rejoin_channel(self, cardinal, user, channel, nick, reason):
+        if not self.rejoin_on_kick:
+            return
 
-def setup(cardinal):
-    return InviteJoinPlugin()
+        if nick == cardinal.nickname:
+            cardinal.join(channel)
+
+
+def setup(cardinal, config):
+    return InviteJoinPlugin(cardinal, config)
