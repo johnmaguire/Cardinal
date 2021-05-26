johnmaguire/Cardinal

commit d670ce26202b9e4cdc6fc06404c47b282c46ddf3

Remove blank paragraphs in Wikipedia summary (closes #187)

plugins/wikipedia/plugin.py
@@ -72,8 +72,13 @@ class WikipediaPlugin:
                 summary = "Disambiguation Page"
             else:
                 content = soup.find_all("div", id="mw-content-text")[0]
-                first_paragraph = content.find(
-                    "p", class_=class_is_not_mw_empty_elt).get_text().strip()
+                for x in content.find_all(
+                            "p",
+                            class_=class_is_not_mw_empty_elt,
+                        ):
+                    if len(x.get_text(strip=True)) != 0:
+                        first_paragraph = x.get_text(strip=True)
+                        break
 
                 if len(first_paragraph) > self._max_description_length:
                     summary = "{}...".format(

commit 2ec84b365a64b8f93c624038f88211e7ce85d043

Fix formatting bug in wikipedia plugin
The fix for #187 in d670ce26202b9e4cdc6fc06404c47b282c46ddf3 introduced
a bug where whitespace was collapsed when reading some entries. This
seems to fix it.

plugins/wikipedia/plugin.py
@@ -77,7 +77,7 @@ class WikipediaPlugin:
                             class_=class_is_not_mw_empty_elt,
                         ):
                     if len(x.get_text(strip=True)) != 0:
-                        first_paragraph = x.get_text(strip=True)
+                        first_paragraph = x.get_text().strip()
                         break
 
                 if len(first_paragraph) > self._max_description_length: