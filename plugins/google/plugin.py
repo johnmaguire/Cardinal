0b8d844423afd3fd845d3517e611fece49eb2f70
John Maguire <contact@johnmaguire.me>
2026-07-08 00:21:57 -0400

    Replace unmaintained google package with ddgs
    The google package broke when Google began requiring JavaScript
    to render results. ddgs is a keyless metasearch library; searches
    now run off the reactor thread and results include titles.

--- plugins/google/plugin.py
+++ plugins/google/plugin.py
@@ -1,49 +1,72 @@
+import logging
+
 from cardinal.decorators import command, help
 
-from googlesearch import search
+from ddgs import DDGS
+from twisted.internet import defer
+from twisted.internet.threads import deferToThread
 
 DEFAULT_MAX_RESULTS = 3
+DEFAULT_BACKEND = 'auto'
+MAX_TITLE_LENGTH = 80
+
+
+def format_result(result):
+    # scraped titles can contain junk whitespace or run absurdly long
+    title = ' '.join(result['title'].split())
+    if len(title) > MAX_TITLE_LENGTH:
+        title = title[:MAX_TITLE_LENGTH - 1] + '…'
+
+    return '{} - {}'.format(title, result['href'])
 
 
 class GoogleSearch:
     def __init__(self, config):
+        self.logger = logging.getLogger(__name__)
+
         config = config if config is not None else {}
         self.max_results = config.get('max_results', DEFAULT_MAX_RESULTS)
+        self.backend = config.get('backend', DEFAULT_BACKEND)
 
+    @defer.inlineCallbacks
     @command(['google', 'lmgtfy', 'g'])
     @help("Returns the URL of the top result for a given search query")
     @help("Syntax: .google <query>")
     def query(self, cardinal, user, channel, msg):
-        # gets search string from message, and makes it url safe
         try:
             search_string = msg.split(' ', 1)[1]
         except IndexError:
-            return cardinal.sendMsg(channel, 'Syntax: .google <query>')
-
-        urls = []
-        counter = self.max_results
-        for url in search(search_string):
-            urls.append(url)
-
-            counter -= 1
-            if counter == 0:
-                break
-
-        if not urls:
-            cardinal.sendMsg(channel, "No results found")
+            cardinal.sendMsg(channel, 'Syntax: .google <query>')
             return
-        elif len(urls) == 1:
+
+        try:
+            results = yield deferToThread(
+                DDGS().text,
+                search_string,
+                backend=self.backend,
+                max_results=self.max_results,
+            )
+        except Exception:
+            self.logger.exception("Error searching for: %s", search_string)
+            cardinal.sendMsg(channel, "Error fetching search results")
+            return
+
+        if not results:
+            cardinal.sendMsg(channel, "No results found")
+        elif len(results) == 1:
             cardinal.sendMsg(channel, "Top result for '{}': {}".format(
                 search_string,
-                urls[0],
+                format_result(results[0]),
             ))
         else:
             cardinal.sendMsg(channel, "Top results for '{}':".format(
                 search_string
             ))
 
-            for url in urls:
-                cardinal.sendMsg(channel, "  {}".format(url))
+            for result in results:
+                cardinal.sendMsg(channel, "  {}".format(
+                    format_result(result),
+                ))
 
 
 entrypoint = GoogleSearch
